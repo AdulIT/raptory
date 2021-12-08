fd6ff09cd9279a5e65121fc27350f1da334469d9
AdulIt <spilberg.max@bk.ru>
2021-12-08 18:37:50 +0600

    Main page

---
+++ dist/js/script.js
@@ -0,0 +1,18 @@
+const search = document.querySelector('#search'),
+      clear = document.querySelector('.clear'),
+      hamburger = document.querySelector('.hamburger'),
+      menu = document.querySelector('.menu'),
+      close = document.querySelector('.menu__close');
+
+
+clear.addEventListener('click', function() {
+    search.value = '';
+});
+
+hamburger.addEventListener('click', () => {
+    menu.classList.add('active');
+});
+
+close.addEventListener('click', () => {
+    menu.classList.remove('active');
+});
